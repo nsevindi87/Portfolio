687a5d8710683e09332cfa24c9a74516b736e87b
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-27 17:32:26 +0200

    JavScript is been written

---
+++ js/style-switcher.js
@@ -0,0 +1,6 @@
+/* =================================== toggle style switcher =================== */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggle.addEventListener("click", () => {
+    console.log("tiklandi")
+})
